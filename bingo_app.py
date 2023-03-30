99dde07c28b1c1d62194d00b6f431b6ea61f5134
Tomer Danon <danont@gmail.com>
2023-03-29 18:10:42 -0600

    updates songs, make_boards, and bingo_app

--- bingo_app.py
+++ bingo_app.py
@@ -4,14 +4,13 @@ import streamlit as st
 import random
 import time
 
-st.title('Biscuits Bingo')
+st.title('Bisco Bingo')
 
 class Bingo():
     def __init__(self):
-        self.songs = ['', '42', '7-11', 'above the waves', 'abraxas', 'aceetobee', 'air song', 'and the ladies were the rest of the night', 'another plan of attack', 'anthem', 'aquatic ape', 'astronaut', 'astronomy domine', 'barfly', 'basis for a day', 'bazaar escape', 'bernstein and chasnoff', 'bombs', 'boom shanker', 'catalyst', 'caterpillar', 'caves of the east', 'chemical warfare brigade', 'clocks', 'commercial amen', 'confrontation', 'crickets', 'crystal ball', 'cyclone', 'dance of the sugar plum fairy', 'digital buddha', 'down to the bottom', 'eulogy', 'everybody needs somebody to love', 'evolve', 'feeling twisted', 'flash mob', 'floes', 'floodlights', 'freeze', 'frog legs', 'grass is green', 'haleakala crater', 'helicopters', 'high speed racer', 'highwire', 'home again', 'hope', 'hot air balloon', 'house dog party favor', "humuhumunukunukuapua'a", 'i remember when', 'i-man', 'jack straw', 'jamillia', 'jigsaw earth', 'kamaole sands', 'king of the world', 'kitchen mitts', 'knights of cydonia', 'konkrete', 'lake shore drive', 'liquid handcuffs', 'little betty boop', 'little lai', 'little shimmy in a conga line', 'loose change', 'lunar pursuit', 'm.e.m.p.h.i.s.', 'm1', 'm80', 'magellan', 'magellan reprise', 'meditation', 'mindless dribble', 'minions', 'miracles', 'mirrors', 'morph dusseldorf', 'moshi fameus', 'mr. don', "mulberry's dream", 'munchkin invasion', 'my lady survives', 'naeba', 'neck romancer', 'new song', 'news from nowhere', 'nughuffer', 'o fortuna', 'on time', 'onamae wa', 'once the fiddler paid', 'one chance to save the world', 'orch theme', 'papercut', 'park ave.', 'pat and dex', "pilin' it high", 'pimp blue rikkis', 'plan b', 'portal to an empty head', 'pygmy twylyte', 'radiator', 'rainbow song', 'resurrection', 'rivers', 'rock candy', 'rock your dance', 'rockafella', 'run like hell', 'running into the night', 'sabre dance', 'save the robots', 'shadow', "she's gone", 'shelby rose', 'shem-rah boo', 'shocked!', "sister judy's soul shack", 'songs of joy', 'sound one', 'space train', 'spacebirdmatingcall', 'spaga', "spaga's last stand", 'spectacle', 'spraypaint', 'spy', 'step inside', 'stone', 'story of the world', 'strobelights and martinis', 'svenghali', 'sweating bullets', 'tamarin alley', 'tempest', 'the big happy', 'the bridge', 'the champions', 'the city', 'the deal', "the devil's waltz", 'the great abyss', 'the overture', 'the safety dance', 'the thieving magpie', 'the truth', 'the tunnel', 'the very moon', 'the wormhole', 'therapy', 'three wishes', 'times square', 'to be continued', 'tourists (rocket ship)', 'tricycle', 'triumph', 'trooper mccue', "trucker's choice", 'twisted in the road', 'uber glue', 'vassillios', 'vibes', 'voices insane', 'we like to party', 'wet', "who's in charge?", 'why we dance', 'widgets', 'widow in the rain', 'wizards in winter', 'world is spinning', 'you and i', 'freebis slinky', 'times square']
-        # pd.read_csv('./songs.csv')['0'].tolist()
+        self.songs = [' ', '7-11'] + pd.read_csv('./songs_full.csv')['0'].tolist()
         self.boards = {
-            'player1':pd.DataFrame([
+            'Jeremy':pd.DataFrame([
                 ['tourists (rocket ship)', "spaga's last stand", 'why we dance', 'twisted in the road', 'another plan of attack'],
                 ['anthem', 'spraypaint', 'jigsaw earth', 'kitchen mitts', 'the overture'],
                 ['therapy', 'mindless dribble', 'times square', 'abraxas', '7-11'],
@@ -32,50 +31,69 @@ class Bingo():
                 ['triumph', 'down to the bottom', 'shem-rah boo', 'strobelights and martinis', 'crystal ball'],
                 ['highwire', 'mindless dribble', 'm1', 'catalyst', '7-11'],
             ]),
-            'player4':pd.DataFrame([
+            'Jon':pd.DataFrame([
                 ['little lai', 'running into the night', 'the great abyss', 'house dog party favor', 'voices insane'],
                 ['uber glue', 'tourists (rocket ship)', '42', 'm1', 'home again'],
                 ["mulberry's dream", 'catalyst', 'crystal ball', 'times square', 'twisted in the road'],
-                ['sound one', '', '', '', ''],
-                ['', '', '', '', ''],
+                ['sound one', 'morph dusseldorf', 'another plan of attack', 'lunar pursuit', 'chemical warfare brigade'],
+                ['shem-rah boo', 'the very moon', 'helicopters', 'naeba', 'spraypaint']
             ]),
-            'player2':pd.DataFrame([
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
+            'Evan':pd.DataFrame([
+                ['evolve', 'mr. don', "humuhumunukunukuapua'a", 'little lai', "who's in charge?"],
+                ['mindless dribble', 'aceetobee', 'jigsaw earth', 'spaga', 'crickets'],
+                ["mulberry's dream", 'another plan of attack', 'helicopters', '42', 'run like hell'],
+                ['the overture', '7-11', 'confrontation', 'house dog party favor', 'space train'],
+                ['tourists (rocket ship)', 'once the fiddler paid', 'little betty boop', 'hope', 'twisted in the road']
             ]),
-            'player2':pd.DataFrame([
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
+            'Tomer':pd.DataFrame([
+                ['another plan of attack', 'rocket science', 'freeze', 'anthem', 'plan b'],
+                ['m1', 'feeling twisted', 'morph dusseldorf', 'tourists (rocket ship)', 'uber glue'],
+                ['vassillios', '42', 'strobelights and martinis', 'spaga', 'magellan'],
+                ['hot air balloon', 'clocks', 'little shimmy in a conga line', 'twisted in the road', 'evolve'],
+                ['why we dance', 'freebis slinky', 'monster', 'triumph', "spaga's last stand"],
             ]),
-            'player2':pd.DataFrame([
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
+            'Raffi':pd.DataFrame([
+                ['why we dance', 'the champions', 'nughuffer', 'little lai', 'world is spinning'],
+                ['shem-rah boo', 'spraypaint', 'crystal ball', 'anthem', 'down to the bottom'],
+                ['mr. don', 'minions', 'times square', 'aquatic ape', 'papercut'],
+                ['pygmy twylyte', 'hope', 'aceetobee', 'abraxas', '7-11'],
+                ['naeba', 'catalyst', 'tourists (rocket ship)', 'lunar pursuit', "spaga's last stand"]
             ]),
-            'player2':pd.DataFrame([
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
+            'player8':pd.DataFrame([
+                ['and the ladies were the rest of the night', 'times square', 'morph dusseldorf', 'naeba', 'bernstein and chasnoff'],
+                ['the champions', 'freeze', 'grass is green', 'i-man', 'space train'],
+                ['why we dance', '42', 'abraxas', 'confrontation', "humuhumunukunukuapua'a"],
+                ['aceetobee', 'crickets', 'crystal ball', 'highwire', 'mindless dribble'],
+                ["mulberry's dream", 'save the robots', 'running into the night', 'spectacle', '7-11']
             ]),
-            'player2':pd.DataFrame([
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
-                ['', '', '', '', ''],
+            'player9':pd.DataFrame([
+                ['42', 'hot air balloon', 'therapy', 'house dog party favor', 'mindless dribble'],
+                ['shem-rah boo', 'bernstein and chasnoff', 'little betty boop', '7-11', 'i-man'],
+                ['morph dusseldorf', 'spraypaint', 'crystal ball', 'rock candy', 'grass is green'],
+                ['helicopters', 'save the robots', "mulberry's dream", 'run like hell', 'tempest'],
+                ['the great abyss', "humuhumunukunukuapua'a", 'spaga', 'little shimmy in a conga line', 'jigsaw earth']
             ]),
-
-            
+            'player10':pd.DataFrame([
+                ['7-11', "spaga's last stand", 'running into the night', 'lunar pursuit', 'anthem'],
+                ['confrontation', 'nughuffer', 'spectacle', 'm1', 'jigsaw earth'],
+                ['morph dusseldorf', 'munchkin invasion', 'naeba', 'abraxas', 'tourists (rocket ship)'],
+                ['spraypaint', 'why we dance', 'shocked!', 'the great abyss', 'i-man'],
+                ['helicopters', '42', 'crystal ball', 'on time', 'times square']
+            ]),
+            'Jesse':pd.DataFrame([
+                ['why we dance', 'highwire', 'm.e.m.p.h.i.s.', 'times square', "spaga's last stand"],
+                ['munchkin invasion', "trucker's choice", 'rock candy', 'space train', 'magellan'],
+                ['little betty boop', 'crystal ball', 'naeba', 'spaga', 'spraypaint'],
+                ['twisted in the road', 'anthem', 'home again', 'mindless dribble', "mulberry's dream"],
+                ['shocked!', 'to be continued', 'freeze', 'the deal', 'another plan of attack']
+            ]),
+            # 'player9':pd.DataFrame([
+            #     ['', '', '', '', ''],
+            #     ['', '', '', '', ''],
+            #     ['', '', '', '', ''],
+            #     ['', '', '', '', ''],
+            #     ['', '', '', '', ''],
+            # ]),  
         }
         self.winners = []
         # ['Rank', 'Player', 'Row/Col/Diag']
@@ -150,7 +168,7 @@ def main():
     b = Bingo()
     count = 0
     x = st.empty()
-    
+
     for i in range(200):
         song = st.sidebar.selectbox(f'Song {count+1}:', b.songs, key=str(count))
         count += 1 
@@ -160,14 +178,14 @@ def main():
             b.check_winner()
             score = b.rank.sort_values(by=['count'], ascending=False)
             score = score[score['count'] != 0]
-            
+
             x.dataframe(score.head(5))
 
     if len(b.winners) > 0:
         st.title('BINGO! The winner(s):')
         st.header(*b.winners.values)
         st.balloons()
-            
+
     show = st.checkbox('show boards')
     if show:
         b.print_boards()
